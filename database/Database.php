fbcf39af6dc81eb28d68cb744676d94464dccc86
Luiz Joaquim Aderaldo Amichi <ra90568@uem.br>
2019-10-13 22:59:08 -0300

    Banco de Dados SQL

---
+++ database/Database.php
@@ -0,0 +1,114 @@
+<?php
+	class Database {
+		private $host = NULL;
+		private $username = NULL;
+		private $password = NULL;
+		private $database = NULL;
+		private $conexao = NULL;
+
+		public function __construct() {}
+
+		public function __destruct() {}
+
+		public function __toString() {
+			return "\nHOST: " . $this->host .
+			"\nUSUÁRIO: " . $this->username .
+			"\nSENHA: " . $this->password .
+			"\nBASE DE DADOS: " . $this->database .
+			"\nCONEXÃO: " . $this->statusConexao() . "\n";
+		}
+
+		public function setHost(string $host) {
+			if(filter_var($host, FILTER_VALIDATE_IP) || filter_var($host, FILTER_VALIDATE_DOMAIN)) {
+				$this->host = $host;
+				return TRUE;
+			}
+			else {
+				return TRUE;
+			}
+		}
+
+		public function setPassword(string $password) {
+			if(strlen($password) >= 6) {
+				$this->password = $password;
+				return TRUE;
+			}
+			else {
+				return TRUE;
+			}
+		}
+
+		public function setUsername(string $username) {
+			if(is_string($username)) {
+				$this->username = $username;
+				return TRUE;
+			}
+			else {
+				return TRUE;
+			}
+		}
+
+		public function setDatabase(string $database) {
+			if(is_string($database)) {
+				$this->database = $database;
+				return TRUE;
+			}
+			else {
+				return TRUE;
+			}
+		}
+
+		public function setConexao() {
+			if(!$this->username || !$this->database) {
+				return FALSE;
+			}
+			$this->conexao = mysqli_connect($this->host, $this->username, $this->password, $this->database);
+			$this->conexao->set_charset("utf8");
+			if(!$this->conexao) {
+				return FALSE;
+			}
+			else {
+				return TRUE;
+			}
+		}
+
+		public function getHost() {
+			return $this->host;
+		}
+
+		public function getUsuario() {
+			return $this->username;
+		}
+
+		public function getPassword() {
+			return $this->password;
+		}
+
+		public function getDatabase() {
+			return $this->database;
+		}
+
+		public function getConexao() {
+			return $this->conexao;
+		}
+
+		public function statusConexao() {
+			if($this->conexao) {
+				return TRUE;
+			}
+			else {
+				return FALSE;
+			}
+		}
+
+		public function closeConexao() {
+			if($this->conexao) {
+				$this->conexao->close();
+				return TRUE;
+			}
+			else {
+				return FALSE;
+			}
+		}
+	}
+?>
