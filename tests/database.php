ff862d7cfa8afa71d1e7a34429a4c8697ddc06ed
Luiz Joaquim Aderaldo Amichi <ra90568@uem.br>
2019-10-21 18:25:18 -0300

    Testes unitários da conexão do PHP com o SGBD

---
+++ tests/database.php
@@ -0,0 +1,16 @@
+<?php
+	setlocale(LC_CTYPE, "pt_BR.UTF8");
+	include_once("../database/Database.php");
+
+	$database = new Database();
+	$database->setHost("127.0.0.1");
+	$database->setUsername("root");
+	$database->setPassword("");
+	$database->setDatabase("graphic_master");
+	$database->setConexao();
+
+	// DEBUG MODE
+	if((isset($argv[1]) && $argv[0] == "database.php" && $argv[1] == "debug") || (isset($argv[1]) && $argv[1] == "debug-all")) {
+		echo $database;
+	}
+?>
