7b115aece6295f4be5475518ce1d1d84b0dc5b73
GustavoBMendes <mendes.gustavob@gmail.com>
2019-10-22 17:16:16 -0300

    set/get cnpj

--- model/Fornecedor.php
+++ model/Fornecedor.php
@@ -34,12 +34,43 @@
 			}
 		}
 
+		public function setCnpj(string $cnpj) {
+			$cnpj = preg_replace("/[^0-9]/", "", (string) $cnpj);
+			if(strlen($cnpj) != 14) {
+				return FALSE;
+			}
+			if(preg_match("/(\d)\1{13}/", $cnpj)) {
+				return FALSE;
+			}
+			for($i = 0, $j = 5, $soma = 0; $i < 12; $i++) {
+				$soma += $cnpj{$i} * $j;
+				$j = ($j == 2) ? 9 : $j - 1;
+			}
+			$resto = $soma % 11;
+			if($cnpj{12} != ($resto < 2 ? 0 : 11 - $resto)) {
+				return FALSE;
+			}
+			for($i = 0, $j = 6, $soma = 0; $i < 13; $i++) {
+				$soma += $cnpj{$i} * $j;
+				$j = ($j == 2) ? 9 : $j - 1;
+			}
+			$resto = $soma % 11;
+			if($cnpj{13} == ($resto < 2 ? 0 : 11 - $resto)) {
+				$this->cnpj = $cnpj;
+			}
+			return TRUE;
+		}
+
 		public function getId() {
 			return $this->id;
 		}
 
 		public function getNome() {
 			return $this->nome;
+		}
+		
+		public function getCnpj() {
+			return $this->cnpj;
 		}
 
 	}
