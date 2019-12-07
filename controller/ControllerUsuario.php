cca85eea020acd4a308e387a0e1f40b212af352b
Luiz Joaquim Aderaldo Amichi <ra90568@uem.br>
2019-12-07 17:10:39 -0300

    Controlador de usuários

---
+++ controller/ControllerUsuario.php
@@ -0,0 +1,78 @@
+<?php
+	include_once("../dao/UsuarioDAO.php");
+	include_once("ControllerPermissao.php");
+	include_once("../model/Usuario.php");
+	class ControllerUsuario {
+		private $dao;
+
+		public function __construct() {}
+
+		public function __destruct() {}
+
+		public function __toString() {
+			return "DAO: " . strtoupper(substr(get_class($this->dao), 0, -3)) . "\n";
+		}
+
+		public function setDao(UsuarioDAO $dao) {
+			if($dao->getDatabase()) {
+				$this->dao = $dao;
+				return TRUE;
+			}
+			else {
+				return FALSE;
+			}
+		}
+
+		public function getDao() {
+			return $this->dao;
+		}
+
+		public function procuraId(int $id) {
+			return $this->dao->searchId($id);
+		}
+
+		public function procuraNome(string $nome) {
+			return $this->dao->searchUsuario($nome);
+		}
+
+		public function procuraLogin(string $login) {
+			return $this->dao->searchLogin($login);
+		}
+
+		public function adicionaUsuario(Usuario $usuario) {
+			return $this->dao->insertUsuario($usuario);
+		}
+
+		public function alteraUsuario(Usuario $usuario) {
+			return $this->dao->updateUsuario($usuario);
+		}
+
+		public function removeUsuario(Usuario $usuario) {
+			return $this->dao->removeUsuario($usuario);
+		}
+
+		public function listarUsuarios(int $numero) {
+			return $this->dao->listUsuario($numero);
+		}
+
+		public function validaUsuario(Usuario $usuario) {
+			$controllerPermissao = new ControllerPermissao();
+			if($usuario->getId() && $usuario->getNome() && $usuario->getLogin() && $usuario->getSenha() && $controllerPermissao->validaPermissao($usuario->getCargo())) {
+				return TRUE;
+			}
+			else {
+				return FALSE;
+			}
+		}
+
+		public function validaLogin(string $login, string $senha) {
+			$usuario = $this->dao->searchLogin($login);
+			if($usuario && password_verify($senha, $usuario->getSenha())) {
+				return TRUE;
+			}
+			else {
+				return FALSE;
+			}
+		}
+	}
+?>
